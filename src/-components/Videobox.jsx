1c9c5c7b54380bc9253a2b659993abefc0e5a5a6
luanntb <luanndebortoli@gmail.com>
2025-02-12 16:29:44 -0300

    Flickster

--- src/-components/Videobox.jsx
+++ src/-components/Videobox.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import styles from "./Videobox.module.css"
 
-const Videobox = () => {
+const Videobox = ( {src, srca }) => {
+
     return(
 
         <div className={styles.container}>
-            <h1>AAAAAAAAA</h1>
+            <div className={styles.info}>
+                <img className={styles.thumbnail} src={src}/>
+            </div>
+            <div className={styles.account}>
+                <img className={styles.pic} src={srca}/>
+                    <styles className={styles.inContainer}>
+                        <span className={styles.videoName}> EXEMPLO DE TÍTULO DE VIDEO </span>
+                        <span className={styles.userName}>Publicadoranonimo562</span>
+                    </styles>
+            </div>
         </div>
     )
 }
--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,169 @@ import React from "react";
 import TopBar from "./-components/TopBar";
 import Sidebar from "./-components/Sidebar";
 import Videobox from "./-components/Videobox";
+import styles from "./-components/App.module.css";
 
 const App = () => {
+
+  const generateImg = () => {
+    return `https://picsum.photos/350/200?random=${Math.random()}`;
+  }
+
+  const generatePic = () => {
+    return `https://picsum.photos/50/50?random=${Math.random()}`
+  }
+
   return(
-    <div className="app">
-      <TopBar /> <Sidebar /> <Videobox />
+    <div className={styles.app}>
+      <TopBar/>
+      <Sidebar/>
+      <div className={styles.content}>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+          <Videobox src={generateImg()} srca={generatePic()}/>
+      </div>
+        
+      
     </div>
   )
 }
